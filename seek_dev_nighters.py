24fd92e5c80b421c4439621e8ac12a067d289cc2
madman61rus <rhjnhjtn3yjhe>
2017-01-10 22:50:48 +0300

    + Переделал функцию load_attempts. Теперь загружаются все страницы из API + Разбил функцию get_midnighters на 2. Теперь проверка на 'полуночность' пользователя в отдельной функции + Использовал yield from вместо цикла + Вынес переменные времени в заголовок файла и сделал их константами + Указал и заморозил используемые библиотеки

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,34 +3,34 @@ import json
 from pytz import timezone
 from datetime import datetime, time
 
-def load_attempts(url,pages=1):
-    for page in range(pages):
-        params = {'page' : pages}
+TIME_MIDNIGHT = time(0,0,0)
+TIME_MORNING = time(6,0,0)
+
+def load_attempts(url):
+    pages = requests.get(url)
+    for page in range(1,pages.json()['number_of_pages']+1):
+        params = {'page' : page}
         response = requests.get(url,params)
         if response.status_code==200:
             users = json.loads(response.text)
-            for user_info in users['records']:
-                yield {
-                    'username': user_info['username'],
-                    'timestamp': user_info['timestamp'],
-                    'timezone': user_info['timezone'],
-                }
+            yield from users['records']
+
+def is_user_midnighter(attempt):
+    if not attempt['timestamp'] is None:
+        utc_time = datetime.fromtimestamp(attempt['timestamp'], tz=timezone(attempt['timezone'])).time()
+        if TIME_MIDNIGHT < utc_time < TIME_MORNING:
+            return True
+        else:
+            return False
 
 def get_midnighters(attempts):
-    time_midnight = time(0,0,0)
-    time_morning = time(6,0,0)
-    midnightes = set()
-    for attempt in attempts:
-        if not attempt['timestamp'] is None:
-            utc_time = datetime.fromtimestamp(attempt['timestamp'],tz=timezone(attempt['timezone'])).time()
-            if time_midnight < utc_time < time_morning:
-                midnightes.add(attempt['username'])
-    return midnightes
+    return {attempt['username'] for attempt in attempts if is_user_midnighter(attempt)}
+
 
 def print_midnighters(midnighters):
     print('Список полуночников : ')
     count = 1
-    for midnighter in list(midnighters):
+    for midnighter in midnighters:
         print('{}. {}'.format(count,midnighter))
         count += 1
 
